b4b27df13137221420566abfaa6fd13b502185a4
wisanu.pljk <wisanu.pljk@gmail.com>
2021-01-12 10:44:26 +0700

    Initial Commit

---
+++ MyBiometrics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyBiometrics
+//
+//  Created by Wisanu Paunglumjeak on 12/1/2564 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
